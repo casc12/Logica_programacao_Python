b2b204313328d4a15c9b4393723f60ec059423c7
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2025-02-10 17:02:33 -0300

    Criado usando o Colab

---
+++ Aula-If-Else.py
@@ -0,0 +1,64 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOFakKOpwvlNCRSylbgDBSi",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/casc12/Logica_programacao_Python/blob/Curso_Programacao/Aula-If-Else.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 14,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "_JD2gfW5aKJr",
+        "outputId": "0e40ce8a-83a3-4e35-8561-dff65243e25f"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Digite sua idade 12\n",
+            "A idade é menor que 18\n"
+          ]
+        }
+      ],
+      "source": [
+        "\"\"\"nome = \"\"\n",
+        "nome = input(\"Digite seu nome\")\n",
+        "print(nome)\n",
+        "print(5*5) \"\"\"\n",
+        "idade = int(input(\"Digite sua idade \"))\n",
+        "# print(idade)\n",
+        "if idade > 18:\n",
+        "  print(\"A idade é maior que 18\")\n",
+        "else:\n",
+        "  print(\"A idade é menor que 18\")\n",
+        "#d"
+      ]
+    }
+  ]
+}
