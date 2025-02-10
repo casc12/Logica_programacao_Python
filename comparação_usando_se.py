61573637d2d339b5c5480c5e8dec656f7b2d7fa1
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2025-02-10 16:56:15 -0300

    Update and rename omparação_usando_se.py to comparação_usando_se.py

---
+++ comparação_usando_se.py
@@ -0,0 +1,9 @@
+"""nome = ""
+nome = input("Digite seu nome")
+print(nome)
+print(5*5) """
+idade = int(input("Digite sua idade "))
+# print(idade)
+if idade > 18:
+  print("A idade é maior que 18")
+#d
