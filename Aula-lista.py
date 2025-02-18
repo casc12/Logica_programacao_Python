45909a319627e7635f08bc34a72a52212ca7fc42
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2025-02-18 18:09:10 -0300

    Update Aula-lista.py

--- Aula-lista.py
+++ Aula-lista.py
@@ -1,59 +1,7 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMq/54zyPyMLQvnUwFK7Ghx",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/casc12/Logica_programacao_Python/blob/Curso_Programacao/Aula-lista.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": 1,
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "_JD2gfW5aKJr",
-        "outputId": "fd5e5b4e-538b-4194-86d7-91c6e47467f7"
-      },
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "['banana', 'maçã', 'abacaxi']\n"
-          ]
-        }
-      ],
-      "source": [
-        "\"\"\"texto = \"Curso de Lógica\"\n",
-        "\n",
-        "for x in texto:\n",
-        "  print(x)\"\"\"\n",
-        "compras = [\"banana\",\"maçã\", \"abacaxi\"]\n",
-        "print(compras)\n",
-        "#d\n"
-      ]
-    }
-  ]
-}
+"""texto = "Curso de Lógica"
+
+for x in texto:
+  print(x)"""
+compras = ["banana","maçã", "abacaxi"]
+print(compras)
+#d
