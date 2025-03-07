fbcc91e21bdb9f45e0a5b2f7917c377db357024a
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2025-03-07 16:19:39 -0300

    Update lista_com_for.py

--- lista_com_for.py
+++ lista_com_for.py
@@ -1,66 +1,9 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMLBgWl4e3xINiuoU54CRQV",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/casc12/Logica_programacao_Python/blob/Curso_Programacao/lista_com_for.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": 4,
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "_JD2gfW5aKJr",
-        "outputId": "664ac925-d3ee-4906-8fe6-712f063637f1"
-      },
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "['banana', 'maçã', 'abacaxi']\n",
-            "maçã\n",
-            "banana\n",
-            "maçã\n",
-            "abacaxi\n"
-          ]
-        }
-      ],
-      "source": [
-        "\"\"\"texto = \"Curso de Lógica\"\n",
-        "\n",
-        "for x in texto:\n",
-        "  print(x)\"\"\"\n",
-        "compras = [\"banana\",\"maçã\", \"abacaxi\"]\n",
-        "print(compras)\n",
-        "print(compras[1])\n",
-        "for x in compras:\n",
-        "  print(x)\n",
-        "#d\n"
-      ]
-    }
-  ]
-}
+"""texto = "Curso de Lógica"
+
+for x in texto:
+  print(x)"""
+compras = ["banana","maçã", "abacaxi"]
+print(compras)
+print(compras[1])
+for x in compras:
+  print(x)
