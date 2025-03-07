1cab8f97feeffd7a1a00facb7ec0ea8d00da4c43
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2025-03-07 16:24:12 -0300

    Criado usando o Colab

---
+++ Lista_com_valores_diferentes.py
@@ -0,0 +1,69 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOoSBF1SNlOrxw6XfmJ+8Qh",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/casc12/Logica_programacao_Python/blob/Curso_Programacao/Lista_com_valores_diferentes.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 5,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "_JD2gfW5aKJr",
+        "outputId": "1bc03ddc-c9a2-4a2f-b460-f14dd122018d"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "['banana', 'maçã', 'abacaxi']\n",
+            "maçã\n",
+            "banana\n",
+            "maçã\n",
+            "abacaxi\n",
+            "['texto', 2, True]\n"
+          ]
+        }
+      ],
+      "source": [
+        "\"\"\"texto = \"Curso de Lógica\"\n",
+        "\n",
+        "for x in texto:\n",
+        "  print(x)\"\"\"\n",
+        "compras = [\"banana\",\"maçã\", \"abacaxi\"]\n",
+        "print(compras)\n",
+        "print(compras[1])\n",
+        "for x in compras:\n",
+        "  print(x)\n",
+        "valores = [\"texto\",2,True]\n",
+        "print(valores)\n",
+        "#d\n"
+      ]
+    }
+  ]
+}
